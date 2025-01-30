794da2bc5e4ba3b26002ef87e30e0d18714706aa
Otavio <maxthedog281@gmail.com>
2025-01-29 22:14:17 -0300

    Update CasaDeAdocao.java

--- Sistema/CasaDeAdocao.java
+++ Sistema/CasaDeAdocao.java
@@ -3,3 +3,28 @@ package Sistema;
 import Animais.Animal;
 import Pessoas.Adotante;
 
+import java.util.ArrayList;
+import java.util.List;
+
+public class CasaDeAdocao {
+  private List<Animal> animaisDisponiveis;
+  private List<Adotante> adotantes;
+
+  public CasaDeAdocao() {
+    this.animaisDisponiveis = new ArrayList<>();
+    this.adotantes = new ArrayList<>();
+  }
+
+  public List<Animal> getAnimaisDisponiveis() {
+    return animaisDisponiveis;
+  }
+  public void setAnimaisDisponiveis(List<Animal> animaisDisponiveis) {
+    this.animaisDisponiveis = animaisDisponiveis;
+  }
+  public List<Adotante> getAdotantes() {
+    return adotantes;
+  }
+  public void setAdotantes(List<Adotante> adotantes) {
+    this.adotantes = adotantes;
+  }
+
