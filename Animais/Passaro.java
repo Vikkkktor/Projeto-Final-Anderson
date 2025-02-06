7ecb546fc6cf2a3c1cb2d3039af80aacd2a1ea06
Viktor Augusto Garbossi Costa <163059297+Vikkkktor@users.noreply.github.com>
2025-02-06 19:22:54 -0300

    Update Passaro.java

--- Animais/Passaro.java
+++ Animais/Passaro.java
@@ -4,13 +4,11 @@ import java.util.Scanner;
 
 public class Passaro extends Animal {
     Scanner scanner = new Scanner(System.in);
-    public Boolean eDomestico;
     public Boolean podeVoar;
     public Boolean podeFalar;
 
-    public Passaro(String nome, String raca, int idade, String porte, String cor, Boolean eDomestico, Boolean podeVoar, Boolean podeFalar) {
-        super(nome, raca, idade, porte, cor);
-        this.eDomestico = eDomestico;
+    public Passaro(String nome, String especie, int idade, String cor, Boolean podeVoar, Boolean podeFalar) {
+        super(nome, especie, idade, cor);
         this.podeVoar = podeVoar;
         this.podeFalar = podeFalar;
     }
@@ -26,12 +24,14 @@ public class Passaro extends Animal {
             System.out.println("PRU PRU");
         }
     }
-    public Boolean getEDomestico() {
-        return eDomestico;
+    public void Voar(){
+        if(getPodeVoar()){
+            System.out.println("voando...");
+        }else{
+            System.out.println("Esse passáro está machucado!");
+        }
     }
-    public void setEDomestico(Boolean eDomestico) {
-        this.eDomestico = eDomestico;
-    }
+    
     public Boolean getPodeVoar() {
         return podeVoar;
     }
