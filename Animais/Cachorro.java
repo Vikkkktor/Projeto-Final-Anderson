ab0374438a4776b8b101059eb546424f9d4fac4e
Viktor Augusto Garbossi Costa <163059297+Vikkkktor@users.noreply.github.com>
2025-02-06 19:19:37 -0300

    Update Cachorro.java

--- Animais/Cachorro.java
+++ Animais/Cachorro.java
@@ -1,10 +1,11 @@
 package Animais;
 
 public class Cachorro extends Animal {
+
     public Boolean eTreinado;
 
-    public Cachorro(String nome, String raca, int idade, String porte, String cor, Boolean eTreinado) {
-        super(nome, raca, idade, porte, cor);
+    public Cachorro(String nome, String especie, int idade, String cor, Boolean eTreinado) {
+        super(nome, especie, idade, cor);
         this.eTreinado = eTreinado;
     }
 
@@ -12,6 +13,14 @@ public class Cachorro extends Animal {
     public void EmitirSons(){
         System.out.println("AuAuAuAu");
     }
+    public void FazerTruques(){
+        if(getETreinado()){
+            System.out.println("Fazendo truques...");
+        }else{
+            System.out.println("Esse Cachorro não é adestrado!");
+        }
+    }
+
     public Boolean getETreinado() {
         return eTreinado;
     }
