b9f8b2549b7694bb2de373298a9e711bd03fdf05
Otavio <maxthedog281@gmail.com>
2025-03-28 09:55:59 -0300

    Update CasaDeAdocao.java

--- Sistema/CasaDeAdocao.java
+++ Sistema/CasaDeAdocao.java
@@ -9,8 +9,9 @@ import java.util.List;
 public interface ProcessoAdocao {
   void adicionarAnimal(Animal animal);
   void registrarAdocao(Adotante adotante);
+}
 
-public class CasaDeAdocao {
+public class CasaDeAdocao implements ProcessoAdocao {
   private List<Animal> animaisDisponiveis;
   private List<Adotante> adotantes;
 
@@ -30,5 +31,17 @@ public class CasaDeAdocao {
   }
   public void setAdotantes(List<Adotante> adotantes) {
     this.adotantes = adotantes;
+  }
+  @Override
+  public void adicionarAnimal(Animal animal) {
+    animaisDisponiveis.add(animal);
+  }
+  
+  @Override
+  public void registrarAdocao(Adotante adotante) {
+    if (animaisDisponiveis.contains(animal)) {
+        animaisDisponiveis.remove(animal);
+        System.out.println(adotante.getNome() + " adotou " + animal.getNome());
+    }
   }
 }
