3fdf93592d019fc205e50c5029201d3459088eef
Otavio <maxthedog281@gmail.com>
2025-04-14 08:54:31 -0300

    Update and rename Teste to CasaDeAdocao.sql

---
+++ MVC/Model/CasaDeAdocao.sql
@@ -0,0 +1,23 @@
+create schema CasaDeAdocao;
+
+use CasaDeAdocao;
+
+create table Animal (
+	ID_Animal int not null auto_increment Primary Key,
+    Nome varchar(50) not null,
+    Especie varchar(50) not null,
+    Idade int not null,
+    Cor varchar(10) not null,
+    Tipo enum ('Cachorro', 'Gato', 'Pássaro')
+);
+
+create table Pessoa (
+	ID_Pessoa int not null auto_increment Primary Key,
+    Nome varchar(50) not null,
+    Endereco varchar(70),
+    Idade int not null,
+    Telefone varchar(16) not null,
+	CPF varchar(14) not null,
+    Funcao enum ('Adotante', 'Funcionário')
+);
+
