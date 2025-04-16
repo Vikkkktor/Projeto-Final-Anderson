e1872dcd591ca7e28e8c6f50f586016dc3bca23e
Otavio <maxthedog281@gmail.com>
2025-04-16 10:38:37 -0300

    Update CasaDeAdocao.sql

--- MVC/Model/CasaDeAdocao.sql
+++ MVC/Model/CasaDeAdocao.sql
@@ -5,11 +5,14 @@ use CasaDeAdocao;
 create table Animal (
 	ID_Animal int not null auto_increment Primary Key,
     Nome varchar(50) not null,
-    Especie varchar(50) not null,
     Idade int not null,
-    Cor varchar(10) not null,
+    Raca varchar(30) not null,
     Adotado boolean default false,
-    Tipo enum ('Cachorro', 'Gato', 'Pássaro') not null
+    Especie enum ('Cachorro', 'Gato', 'Pássaro') not null,
+    -- Atributos especificos para por espécie
+    Pode_Voar boolean null comment 'Apenas para pássaros',
+    Pode_Falar boolean null comment 'Apenas para pássaros',
+    E_Treinado boolean null comment 'Apenas para cachorros'
 );
 
 create table Pessoa (
@@ -19,5 +22,15 @@ create table Pessoa (
     Idade int not null,
     Telefone varchar(16) not null,
 	CPF varchar(14) not null unique,
-    Funcao enum ('Adotante', 'Funcionário') not null
+    Funcao enum ('Adotante', 'Funcionário') not null,
+    Carteira_Adotante varchar(20) null comment 'Apenas para Adotante'
+);	
+
+create table Adocao (
+	ID_Adocao int not null auto_increment Primary Key,
+    ID_Animal int not null,
+    ID_Pessoa int not null,
+    Data_Adocao date not null,
+    foreign key (ID_Animal) references Animal(ID_Animal),
+    foreign key (ID_Pessoa) references Pessoa(ID_Pessoa)
 );
