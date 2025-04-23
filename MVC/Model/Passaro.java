b8d3c2fdbcfd592a397c8c2f1805b7e2548422ec
Otavio <maxthedog281@gmail.com>
2025-04-22 23:54:00 -0300

    Update Passaro.java

--- MVC/Model/Passaro.java
+++ MVC/Model/Passaro.java
@@ -1,47 +1,30 @@
 package Animais;
 
-import java.util.Scanner;
-
 public class Passaro extends Animal {
-    Scanner scanner = new Scanner(System.in);
-    public Boolean podeVoar;
-    public Boolean podeFalar;
+    private boolean podeVoar;
+    private boolean podeFalar;
 
-    public Passaro(String nome, String especie, int idade, String cor, Boolean podeVoar, Boolean podeFalar) {
-        super(nome, especie, idade, cor);
+    // Construtores
+    public Passaro(String nome, int idade, String raca, boolean podeVoar, boolean podeFalar) {
+        super(nome, idade, raca);
         this.podeVoar = podeVoar;
         this.podeFalar = podeFalar;
     }
 
-
-    @Override
-    public void EmitirSons(){
-        if(podeFalar){
-            String imitacao = scanner.nextLine();
-            System.out.println(imitacao);
-        }
-        else{
-            System.out.println("PRU PRU");
-        }
-    }
-    public void Voar(){
-        if(getPodeVoar()){
-            System.out.println("voando...");
-        }else{
-            System.out.println("Esse passáro está machucado!");
-        }
-    }
-    
-    public Boolean getPodeVoar() {
+    // Getters and setters
+    public boolean isPodeVoar() {
         return podeVoar;
     }
-    public void setPodeVoar(Boolean podeVoar) {
+
+    public void setPodeVoar(boolean podeVoar) {
         this.podeVoar = podeVoar;
     }
-    public Boolean getPodeFalar() {
+
+    public boolean isPodeFalar() {
         return podeFalar;
     }
-    public void setPodeFalar(Boolean podeFalar) {
+
+    public void setPodeFalar(boolean podeFalar) {
         this.podeFalar = podeFalar;
     }
 }
