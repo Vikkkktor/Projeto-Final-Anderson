9ad592a44e873402f4123e9daeec00bda7225103
Otavio <maxthedog281@gmail.com>
2025-04-22 23:29:59 -0300

    Create Adocao.java

---
+++ MVC/Model/Adocao.java
@@ -0,0 +1,41 @@
+import java.time.LocalDate;
+
+public class Adocao {
+    private int idAdocao;
+    private int idAnimal;
+    private int idPessoa;
+    private LocalDate dataAdocao;
+
+    // Getters e Setters
+    public int getIdAdocao() {
+        return idAdocao;
+    }
+
+    public void setIdAdocao(int idAdocao) {
+        this.idAdocao = idAdocao;
+    }
+
+    public int getIdAnimal() {
+        return idAnimal;
+    }
+
+    public void setIdAnimal(int idAnimal) {
+        this.idAnimal = idAnimal;
+    }
+
+    public int getIdPessoa() {
+        return idPessoa;
+    }
+
+    public void setIdPessoa(int idPessoa) {
+        this.idPessoa = idPessoa;
+    }
+
+    public LocalDate getDataAdocao() {
+        return dataAdocao;
+    }
+
+    public void setDataAdocao(LocalDate dataAdocao) {
+        this.dataAdocao = dataAdocao;
+    }
+}
