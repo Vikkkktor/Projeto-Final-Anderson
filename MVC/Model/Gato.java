c67b129300a00544d51868838d14662b87ff7bf8
Otavio <maxthedog281@gmail.com>
2025-04-22 23:53:40 -0300

    Update Gato.java

--- MVC/Model/Gato.java
+++ MVC/Model/Gato.java
@@ -1,14 +1,8 @@
 package Animais;
 
 public class Gato extends Animal {
-
-    public Gato(String nome, String especie, int idade, String cor) {
-        super(nome, especie, idade, cor);
+    // Construtores
+    public Gato(String nome, int idade, String raca) {
+        super(nome, idade, raca);
     }
-
-    @Override
-    public void EmitirSons(){
-        System.out.println("Miau Miau");
-    }
-
 }
