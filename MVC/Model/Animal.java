63eca9b82d5aafcaab4ba6e9131528e5660cc4ab
Otavio <maxthedog281@gmail.com>
2025-04-24 08:01:49 -0300

    Update Animal.java

--- MVC/Model/Animal.java
+++ MVC/Model/Animal.java
@@ -3,6 +3,7 @@ package Animais;
 public class Animal {
     private int idAnimal;
     private String nome;
+    private String descricao;
     private int idade;
     private String raca;
     private boolean adotado;
