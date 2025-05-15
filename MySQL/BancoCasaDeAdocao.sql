5f61307682aa69d88b40cd814f768f4ed0f8b511
Otavio <maxthedog281@gmail.com>
2025-05-15 09:51:30 -0300

    Update BancoCasaDeAdocao.sql

--- MySQL/BancoCasaDeAdocao.sql
+++ MySQL/BancoCasaDeAdocao.sql
@@ -7,26 +7,26 @@ create table Animal (
     	Nome varchar(50) not null,
 	CaminhoImg varchar(255),
 	Descricao varchar(200),
-    	Idade int not null,
+    	data_nas DATE,
    	Raca varchar(30) not null,
     	Adotado boolean default false,
 );
 
 create table Cachorro (
-	ID_Cachorro int Primary Key,
+	ID_Cachorro int not null auto_increment Primary Key,
 	E_Treinado Boolean,
 	Foreign Key (ID_Cachorro) references Animal(ID_Animal)
 );
 
 create table Passaro (
-	ID_Passaro int Primary Key,
+	ID_Passaro int not null auto_increment Primary Key,
 	Pode_Voar boolean,
 	Pode_Falar boolean,
 	Foreign Key (ID_Passaro) references Animal(ID_Animal)
 );
 
 create table Gato (
-	ID_Gato int Primary Key,
+	ID_Gato int not null auto_increment Primary Key,
 	Foreign Key (ID_Gato) references Animal(ID_Animal)
 );
 	
@@ -34,19 +34,19 @@ create table Pessoa (
 	ID_Pessoa int not null auto_increment Primary Key,
     	Nome varchar(50) not null,
     	Endereco varchar(70),
-    	Idade int not null,
+    	date_nas DATE not null,
    	Telefone varchar(16) not null,
 	CPF varchar(14) unique,
 );	
 
 create table Adotante (
-	ID_Adotante int Primary Key,
+	ID_Adotante int not null Primary Key,
 	Carteira_Adotante varchar(20),
 	Foreign Key (ID_Adotante) references Pessoa(ID_Pessoa)
 );
 
 create table Funcionario (
-	ID_Funcionario int Primary Key,
+	ID_Funcionario int not null auto_increment Primary Key,
 	Matricula varchar(20),
 	Foreign Key (ID_Funcionario) references Pessoa(ID_Pessoa)
 );
