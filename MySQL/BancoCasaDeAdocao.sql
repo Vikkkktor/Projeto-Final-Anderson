637ac7a0b9d4e659ff914631bca50c7d11952fae
Otavio <maxthedog281@gmail.com>
2025-05-28 15:09:03 -0300

    Update BancoCasaDeAdocao.sql

--- MySQL/BancoCasaDeAdocao.sql
+++ MySQL/BancoCasaDeAdocao.sql
@@ -3,59 +3,59 @@ create schema CasaDeAdocao;
 use CasaDeAdocao;
 
 create table Animal (
-	ID_Animal int not null auto_increment Primary Key,
-    	Nome varchar(50) not null,
-	CaminhoImg varchar(255),
-	Descricao varchar(200),
-    	data_nas DATE,
-   	Raca varchar(30) not null,
-    	Adotado boolean default false,
+    ID_Animal int not null auto_increment Primary Key,
+    Nome varchar(50) not null,
+    CaminhoImg varchar(255),
+    Descricao varchar(200),
+    data_nas DATE,
+    Raca varchar(30) not null,
+    Adotado boolean default false 
 );
 
 create table Cachorro (
-	ID_Cachorro int not null auto_increment Primary Key,
-	E_Treinado Boolean,
-	Foreign Key (ID_Cachorro) references Animal(ID_Animal)
+    ID_Cachorro int not null Primary Key, 
+    E_Treinado Boolean,
+    Foreign Key (ID_Cachorro) references Animal(ID_Animal)
 );
 
 create table Passaro (
-	ID_Passaro int not null auto_increment Primary Key,
-	Pode_Voar boolean,
-	Pode_Falar boolean,
-	Foreign Key (ID_Passaro) references Animal(ID_Animal)
+    ID_Passaro int not null Primary Key, 
+    Pode_Voar boolean,
+    Pode_Falar boolean,
+    Foreign Key (ID_Passaro) references Animal(ID_Animal)
 );
 
 create table Gato (
-	ID_Gato int not null auto_increment Primary Key,
-	Foreign Key (ID_Gato) references Animal(ID_Animal)
+    ID_Gato int not null Primary Key, 
+    Foreign Key (ID_Gato) references Animal(ID_Animal)
 );
-	
+    
 create table Pessoa (
-	ID_Pessoa int not null auto_increment Primary Key,
-    	Nome varchar(50) not null,
-    	Endereco varchar(70),
-    	date_nas DATE not null,
-   	Telefone varchar(16) not null,
-	CPF varchar(14) unique,
-);	
+    ID_Pessoa int not null auto_increment Primary Key,
+    Nome varchar(50) not null,
+    Endereco varchar(70),
+    date_nas DATE not null,
+    Telefone varchar(16) not null,
+    CPF varchar(14) unique
+);    
 
 create table Adotante (
-	ID_Adotante int not null Primary Key,
-	Carteira_Adotante varchar(20),
-	Foreign Key (ID_Adotante) references Pessoa(ID_Pessoa)
+    ID_Adotante int not null Primary Key,
+    Carteira_Adotante varchar(20),
+    Foreign Key (ID_Adotante) references Pessoa(ID_Pessoa)
 );
 
 create table Funcionario (
-	ID_Funcionario int not null auto_increment Primary Key,
-	Matricula varchar(20),
-	Foreign Key (ID_Funcionario) references Pessoa(ID_Pessoa)
+    ID_Funcionario int not null Primary Key,
+    Matricula varchar(20),
+    Foreign Key (ID_Funcionario) references Pessoa(ID_Pessoa)
 );
 
 create table Adocao (
-	ID_Adocao int not null auto_increment Primary Key,
+    ID_Adocao int not null auto_increment Primary Key,
     ID_Animal int not null,
-    ID_Pessoa int not null,
+    ID_Adotante int not null,
     Data_Adocao date not null,
     foreign key (ID_Animal) references Animal(ID_Animal),
-    foreign key (ID_Pessoa) references Pessoa(ID_Pessoa)
+    foreign key (ID_Adotante) references Adotante(ID_Adotante)
 );
